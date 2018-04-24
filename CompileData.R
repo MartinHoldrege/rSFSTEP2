308385ccea7b22c0f0146a3527588ca0408b5bf8
Kyle Palmquist <kapalmqu@yahoo.com>
2018-04-24 17:06:23 -0600

    Resolving issue #22
    Edited compilation file that combines individual site SQLite databases into 1 database so it is compatible with new SOILWAT2 output format.

--- CompileData.R
+++ CompileData.R
@@ -17,56 +17,31 @@ g<-sites[1]
 input_database<-paste("Output_site_",g,".sqlite",sep="")
 con<-dbConnect(SQLite(), input_database)
 
-total_dy<-data.frame(dbGetQuery(con,'select * from total_dy'))
-
-total_wk<-data.frame(dbGetQuery(con,'select * from total_wk'))
-
-total_mo<-data.frame(dbGetQuery(con,'select * from total_mo'))
-
-total_yr<-data.frame(dbGetQuery(con,'select * from total_yr'))
-
 total_bmass<-data.frame(dbGetQuery(con,'select * from total_bmass'))
-
 total_mort<-data.frame(dbGetQuery(con,'select * from total_mort'))
+total_sw2_yearly_slyrs<-data.frame(dbGetQuery(con,'select * from total_sw2_yearly_slyrs'))
+total_sw2_yearly<-data.frame(dbGetQuery(con,'select * from total_sw2_yearly'))
 
 dbDisconnect(con)
-
 
 g<-sites[2]
 
 input_database<-paste("Output_site_",g,".sqlite",sep="")
 con<-dbConnect(SQLite(),input_database)
 
-total_dy_2<-data.frame(dbGetQuery(con,'select * from total_dy'))
-
-total_wk_2<-data.frame(dbGetQuery(con,'select * from total_wk'))
-
-total_mo_2<-data.frame(dbGetQuery(con,'select * from total_mo'))
-
-total_yr_2<-data.frame(dbGetQuery(con,'select * from total_yr'))
-
 total_bmass_2<-data.frame(dbGetQuery(con,'select * from total_bmass'))
-
 total_mort_2<-data.frame(dbGetQuery(con,'select * from total_mort'))
+total_sw2_yearly_slyrs_2<-data.frame(dbGetQuery(con,'select * from total_sw2_yearly_slyrs'))
+total_sw2_yearly_2<-data.frame(dbGetQuery(con,'select * from total_sw2_yearly'))
 
 dbDisconnect(con)
 
-
-total_dy_3<-rbind(total_dy,total_dy_2)
-
-total_wk_3<-rbind(total_wk,total_wk_2)
-
-total_mo_3<-rbind(total_mo,total_mo_2)
-
-total_yr_3<-rbind(total_yr,total_yr_2)
-
 total_bmass_3<-rbind(total_bmass,total_bmass_2)
-
 total_mort_3<-rbind(total_mort,total_mort_2)
+total_sw2_yearly_slyrs_3<-rbind(total_sw2_yearly_slyrs,total_sw2_yearly_slyrs_2)
+total_sw2_yearly_3<-rbind(total_sw2_yearly,total_sw2_yearly_2)
 
-dbWriteTable(db, "total_dy_3", total_dy_3, append=T)
-dbWriteTable(db, "total_wk_3", total_wk_3, append=T)
-dbWriteTable(db, "total_mo_3", total_mo_3, append=T)
-dbWriteTable(db, "total_yr_3", total_yr_3, append=T)
 dbWriteTable(db, "total_bmass_3", total_bmass_3, append=T)
 dbWriteTable(db, "total_mort_3", total_mort_3, append=T)
+dbWriteTable(db, "total_sw2_yearly_slyrs_3",total_sw2_yearly_slyrs_3, append=T)
+dbWriteTable(db, "total_sw2_yearly_3",total_sw2_yearly_3, append=T)
