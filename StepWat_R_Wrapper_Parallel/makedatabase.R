f01059b128981e78bf74251c65da975452d0c8e7
kpalmqui <kpalmqui>
2018-05-28 14:09:06 -0600

    Addressing issue #20
    Added code that aggregates raw daily SOILWAT2 output to mean values for each DOY across all years.

--- StepWat_R_Wrapper_Parallel/makedatabase.R
+++ StepWat_R_Wrapper_Parallel/makedatabase.R
@@ -36,11 +36,23 @@ setwd(directory)
   	total_sw2_yearly_slyrs=read.csv('total_sw2_yearly_slyrs.csv',header=T)
     total_sw2_yearly=read.csv('total_sw2_yearly.csv',header=T)
 
-    #default is to not put daily output into SQLite database as is currently too large. Once we have added aggregation of the daily output to rSFSTEP2, we will want daily output in the SQLite database as well.
-    #total_sw2_daily_slyrs<-read.csv('total_sw2_daily_slyrs.csv',header=T)
-    #total_sw2_daily<-read.csv('total_sw2_daily.csv',header=T)    
+    total_sw2_daily_slyrs<-read.csv('total_sw2_daily_slyrs.csv',header=T)
+    total_sw2_daily<-read.csv('total_sw2_daily.csv',header=T)    
     
+    #calculate aggregated daily mean and sd - need to get the columns after data - site, GCM, soilType, dist_flag, dist_freq, graz_freq, intensity, RCP, YEARS
+	length=length(total_sw2_daily_slyrs[2,])-10
+
+	total_sw2_daily_slyrs_aggregated=aggregate(total_sw2_daily_slyrs[,c(3:length)],by=list(total_sw2_daily_slyrs$Day,total_sw2_daily_slyrs$site,total_sw2_daily_slyrs$GCM, total_sw2_daily_slyrs$soilType,total_sw2_daily_slyrs$dist_flag,total_sw2_daily_slyrs$dist_freq,total_sw2_daily_slyrs$graz_freq,total_sw2_daily_slyrs$intensity,total_sw2_daily_slyrs$RCP,total_sw2_daily_slyrs$YEARS),mean)
+ 	names(total_sw2_daily_slyrs_aggregated)[1:10]=c("Day","site","GCM","soilType","dist_flag","dist_freq","graz_freq","intensity","RCP","YEARS")
+    
+	length=length(total_sw2_daily[2,])-10
+
+	total_sw2_daily_aggregated=aggregate(total_sw2_daily[,c(3:length)],by=list(total_sw2_daily$Day,total_sw2_daily$site,total_sw2_daily$GCM, total_sw2_daily$soilType,total_sw2_daily$dist_flag,total_sw2_daily$dist_freq,total_sw2_daily$graz_freq,total_sw2_daily$intensity,total_sw2_daily$RCP,total_sw2_daily$YEARS),mean)
+ 	names(total_sw2_daily_aggregated)[1:10]=c("Day","site","GCM","soilType","dist_flag","dist_freq","graz_freq","intensity","RCP","YEARS")
+
     #write all tables to the SQLite database
+    dbWriteTable(db, "total_sw2_daily_slyrs",total_sw2_daily_slyrs_aggregated, 	append=T)
+    dbWriteTable(db, "total_sw2_daily",total_sw2_daily_aggregated, append=T)
     dbWriteTable(db, "total_sw2_yearly_slyrs",total_sw2_yearly_slyrs, append=T)
     dbWriteTable(db, "total_sw2_yearly",total_sw2_yearly, append=T)
     dbWriteTable(db, "total_bmass", total_bmass, append=T)
