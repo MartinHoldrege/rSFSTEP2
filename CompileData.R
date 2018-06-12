9d6dcf34eebdfc9c2cfe9a49788318853a675081
kpalmqui <kpalmqui>
2018-06-12 11:53:28 -0600

    Updated script that compiles databases
    - created a loop to loop over all databases and compile
    
    - added code to add an index to the SQLite database
    
    - added in option to request 128GB nodes on Mt. Moran in sample.sh, which is required for runs with 100 iterations.

--- CompileData.R
+++ CompileData.R
@@ -5,43 +5,67 @@
 library(plyr)
 library(RSQLite)
 
-source.dir<-"" #Location where the databases are located, needs to be set by the user
+dir_db<-"" #Location where the databases are located, needs to be set by the user
 
-setwd(source.dir)
+setwd(dir_db)
 output_database<-paste("Output_Compiled",".sqlite",sep="")
 db<-dbConnect(SQLite(),output_database)
-sites<-c(14,103) #Add the id of sites to be compiled
+sites<-c(14,103) #Add the id of all sites to be compiled, 14 and 103 are here as examples
 
-g<-sites[1]
-
-input_database<-paste("Output_site_",g,".sqlite",sep="")
-con<-dbConnect(SQLite(), input_database)
-
-total_bmass<-data.frame(dbGetQuery(con,'select * from total_bmass'))
-total_mort<-data.frame(dbGetQuery(con,'select * from total_mort'))
-total_sw2_yearly_slyrs<-data.frame(dbGetQuery(con,'select * from total_sw2_yearly_slyrs'))
-total_sw2_yearly<-data.frame(dbGetQuery(con,'select * from total_sw2_yearly'))
-
-dbDisconnect(con)
-
-g<-sites[2]
+for (i in 1:length(sites)) {
+g<-sites[i]
 
 input_database<-paste("Output_site_",g,".sqlite",sep="")
 con<-dbConnect(SQLite(),input_database)
 
-total_bmass_2<-data.frame(dbGetQuery(con,'select * from total_bmass'))
-total_mort_2<-data.frame(dbGetQuery(con,'select * from total_mort'))
-total_sw2_yearly_slyrs_2<-data.frame(dbGetQuery(con,'select * from total_sw2_yearly_slyrs'))
-total_sw2_yearly_2<-data.frame(dbGetQuery(con,'select * from total_sw2_yearly'))
+total_bmass_g<-data.frame(dbGetQuery(con,'select * from total_bmass'))
+total_sw2_yearly_slyrs_g<-data.frame(dbGetQuery(con,'select * from total_sw2_yearly_slyrs'))
+total_sw2_yearly_g<-data.frame(dbGetQuery(con,'select * from total_sw2_yearly'))
 
 dbDisconnect(con)
 
-total_bmass_3<-rbind(total_bmass,total_bmass_2)
-total_mort_3<-rbind(total_mort,total_mort_2)
-total_sw2_yearly_slyrs_3<-rbind(total_sw2_yearly_slyrs,total_sw2_yearly_slyrs_2)
-total_sw2_yearly_3<-rbind(total_sw2_yearly,total_sw2_yearly_2)
+dbWriteTable(db, "total_bmass", total_bmass_g, append=T)
+dbWriteTable(db, "total_sw2_yearly_slyrs",total_sw2_yearly_slyrs_g, append=T)
+dbWriteTable(db, "total_sw2_yearly",total_sw2_yearly_g, append=T)
 
-dbWriteTable(db, "total_bmass_3", total_bmass_3, append=T)
-dbWriteTable(db, "total_mort_3", total_mort_3, append=T)
-dbWriteTable(db, "total_sw2_yearly_slyrs_3",total_sw2_yearly_slyrs_3, append=T)
-dbWriteTable(db, "total_sw2_yearly_3",total_sw2_yearly_3, append=T)
+}
+
+#Add index to the compiled database
+#Get names of database(s) to add index to
+fname_dbs <- list.files(dir_db, pattern = "sqlite", full.names = TRUE)
+
+#Local functions utilized below
+dbConnect_OutputDB <- function(fname = NULL, dir = NULL, site = NULL) {
+  fname <- get_OutputDB_filenames(fname, dir, site)[1]
+
+  if (file.exists(fname)) {
+    DBI::dbConnect(RSQLite::SQLite(), fname)
+  } else NULL
+}
+
+add_index <- function(con) {
+  prev_indices <- DBI::dbGetQuery(con, "SELECT * FROM sqlite_master WHERE type = 'index'")
+
+  if (NROW(prev_indices) > 0 && "MZ_exp" %in% prev_indices[, "name"])
+    return(NULL)
+
+  DBI::dbGetQuery(con, paste("CREATE INDEX MZ_exp ON total_bmass (site, GCM, YEARS,",
+    "soilType, intensity, dist_freq)"))
+}
+
+get_OutputDB_filenames <- function(fnames = NULL, dir = NULL, sites = NULL) {
+  if (!is.null(dir) && !is.null(sites)) {
+    file.path(dir, paste0("Output_site_", sites, ".sqlite"))
+
+  } else if (all(sapply(fnames, file.exists))) {
+    fnames
+  } else NULL
+}
+
+#Finally add indices to DBs
+temp <- lapply(fname_dbs, function(fdb) {
+  print(paste(Sys.time(), "add index to", basename(fdb)))
+  con <- dbConnect_OutputDB(fdb)
+  temp <- add_index(con)
+  DBI::dbDisconnect(con)
+})
