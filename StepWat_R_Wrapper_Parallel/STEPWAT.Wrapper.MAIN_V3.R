a21a0d66d5d6e17242dd3a814d1c111392fa2504
kpalmqui <kpalmqui>
2018-09-10 13:04:06 -0600

    Removed outdated, unnecessary comments

--- StepWat_R_Wrapper_Parallel/STEPWAT.Wrapper.MAIN_V3.R
+++ StepWat_R_Wrapper_Parallel/STEPWAT.Wrapper.MAIN_V3.R
@@ -272,29 +272,27 @@ climate.conditions <- c(climate.ambient,  "RCP45.CanESM2", "RCP45.CESM1-CAM5", "
 temp <- climate.conditions[!grepl(climate.ambient, climate.conditions)] #make sure 'climate.ambient' is first entry
 if(length(temp) > 0){
 
+#use with Vic weather database and all new weather databases
 if(database_name!="dbWeatherData_Sagebrush_KP.v3.2.0.sqlite")
 {
-    #use with Vic weather database and all new weather databases
     #Difference between start and end year(if you want 2030-2060 use 50; if you want 2070-2100 use 90 below)
     deltaFutureToSimStart_yr <- c("d50","d90")
-    #use with Vic weather database and all new weather databases
+    
     #Downscaling method
     downscaling.method <- c("hybrid-delta-3mod")
-    #use with Vic weather database and all new weather databases
     temp <- paste0(deltaFutureToSimStart_yr, "yrs.", rep(temp, each=length(deltaFutureToSimStart_yr)))	#add (multiple) deltaFutureToSimStart_yr
+    
     #Set Years
-    #use with Vic weather database and all new weather databases
     YEARS<-c("d50yrs","d90yrs")
   }
   else
   {
-    #use with KP weather database
     #Difference between start and end year(if you want 2030-2060 use 50; if you want 2070-2100 use 90 below)
     deltaFutureToSimStart_yr <- c(50,90)
-    #use with KP weather database
+   
     #Downscaling method
     downscaling.method <- c("hybrid-delta")
-    #use with KP weather database
+   
     temp <- paste0(deltaFutureToSimStart_yr, "years.", rep(temp, each=length(deltaFutureToSimStart_yr)))	#add (multiple) deltaFutureToSimStart_yr
     #Set Years
     #use with KP weather database
@@ -389,7 +387,6 @@ directory<-source.dir
 GCM<-c("Current","CanESM2","CESM1-CAM5","CSIRO-Mk3-6-0","FGOALS-g2","FGOALS-s2","GISS-E2-R","HadGEM2-CC","HadGEM2-ES","inmcm4", "IPSL-CM5A-MR", "MIROC5", "MIROC-ESM", "MRI-CGCM3")
 #Set RCPs
 RCP<-c("RCP45","RCP85")
-#Set Years
 
 #Disturbance Flag, turn to "F" if not using disturbances (grazing,fire)
 dist.graz.flag<-T
