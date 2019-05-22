17fa2c2a2c8f9a94463e9403e6c6d99a63836ece
Kyle Palmquist <kapalmqu@yahoo.com>
2019-05-22 11:52:53 -0600

    Added new argument to dbW_estimate_WGen_coefs, resolving #137
    -add new argument "imputation_type" to function dbW_estimate_WGen_coefs
    -add documentation of new implemenation in WeatherGeneratorInputFile.R

--- R_program/MarkovWeatherFileGenerator.R
+++ R_program/MarkovWeatherFileGenerator.R
@@ -1,5 +1,7 @@
 #Markov script that generates two site-specific files (mkv_covar.in and mkv_prob.in) required by the markov weather generator in SOILWAT2
 
+#Three options for imputation of missing values due to insufficient weather data within function dbW_estimate_WGen_coefs via argument imputation_type: no imputation ("none"), imputation via mean from values before and after the missing value ("meanX"), and imputation by last-observation-carried-forward ("locf"). See function dbW_estimate_WGen_coefs in rSOILWAT2 for additional details.
+
 #Set up system for parallel processing
 library(doParallel)
 registerDoParallel(proc_count)
@@ -12,10 +14,12 @@ registerDoParallel(proc_count)
   foreach (h = 1:H) %dopar%
     { #h = number of GCM X RCP X Times (scenarios)
     scen<-temp[h] #load a particular scenario
-
-    res <- rSOILWAT2::dbW_estimate_WGen_coefs(sw_weatherList[[s]][[h]],
+    
+    #Generate the necessary precipitation and temperature parameters for mkv_covar.in and mkv_prob.in based on historical weather data
+    res <- rSOILWAT2::dbW_estimate_WGen_coefs(sw_weatherList[[s]][[h]], imputation_type = "mean5",
       na.rm = TRUE)
-
+    
+    #Write the mkv_covar.in and mkv_prob.in files
     print_mkv_files(mkv_doy = res[["mkv_doy"]], mkv_woy = res[["mkv_woy"]],
       path = file.path(assembly_output, paste0("Site","_",site,"/","Site", "_", site, "_", scen)))
  }
