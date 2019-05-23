46c1268047a127623a84464b23653c0fc76fd726
chaukap <chaukap@uwyo.edu>
2019-05-23 13:51:53 -0600

    Split generate_stepwat_sites.sh into two functions
    (issue #141)
    * clone_STEPWAT2.sh clones STEPWAT2 into the R_program folder, compiles it, then removed unnecessary files.
    * generate_rSFSTEP2_structure.sh generates the separate R_program_i files and assigns the site.
    * I also removed generate_stepwat_sites.sh

---
+++ cloneSTEPWAT2.sh
@@ -0,0 +1,25 @@
+#Clones STEPWAT2, compiles it, and then removes any files that are not necessary for running rSFSTEP2
+
+cd R_program
+#module load git/2.17.1
+wait
+git clone --branch master --recursive https://github.com/DrylandEcology/STEPWAT2.git
+wait
+cd STEPWAT2
+make
+wait
+#remove everything that is not needed after the make
+rm -rf .git*
+rm *.c
+rm *.h
+rm -rf tools
+rm -rf sw_src
+rm -rf obj
+rm README.md
+rm appveyor.yml
+rm DLM_change_log.txt
+rm makefile
+rm stepwat_test_job.sh
+rm -rf sqlite-amalgamation
+wait
+cd ../..
