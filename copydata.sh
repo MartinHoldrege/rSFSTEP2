14d7486489b306c8faa52dd61da74e8fe841513b
Martin Holdrege <martinholdrege@gmail.com>
2020-12-28 23:00:32 -0700

    settings for 1 site 2 scenerio interactive run on chpc

--- copydata.sh
+++ copydata.sh
@@ -1,7 +1,7 @@
 #Script to copy all Output_site_databases to a particular location
 #!/bin/bash
-site=51
-path=/project/sagebrush/kpalmqu1/sitedata/
+site=1
+path=~/stepwat/sitedata/
 for ((i=1;i<=$site;i++));do (
 	cd R_program_$i
 	mv Output_site* $path
