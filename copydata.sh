c07bc15ed256fc17db5639728a3fa57cd0aa60b2
Martin Holdrege <martinholdrege@gmail.com>
2021-03-22 17:18:44 -0600

    updated to run for 200 sites

--- copydata.sh
+++ copydata.sh
@@ -1,6 +1,6 @@
 #Script to copy all Output_site_databases to a particular location
 #!/bin/bash
-site=4
+site=200
 path=/uufs/chpc.utah.edu/common/home/kulmatiski-group1/stepwat/sitedata/
 for ((i=1;i<=$site;i++));do (
 	cd R_program_$i
