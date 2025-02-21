814a62a1fe939a171665a07a570b7adc9c0b9539
Martin Holdrege <martinholdrege@gmail.com>
2025-02-21 16:39:04 -0600

    200 sites, default

--- copydata.sh
+++ copydata.sh
@@ -1,7 +1,11 @@
 #Script to copy all Output_site_databases to a particular location
 #!/bin/bash
-site=14
-path=../../results/testrun_14sites/
+site=200
+path=../../results/temp2rename/
+
+# Ensure the directory exists
+mkdir -p "$path"
+
 for ((i=1;i<=$site;i++));do (
 	cd R_program_$i
 	mv Output_site* $path
