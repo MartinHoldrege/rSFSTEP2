958645acb12fdfbb7599d3de134c704910fa26e2
Martin Holdrege <martinholdrege@gmail.com>
2025-02-21 16:27:13 -0600

    safer mv of sql files

--- copydata.sh
+++ copydata.sh
@@ -1,7 +1,7 @@
 #Script to copy all Output_site_databases to a particular location
 #!/bin/bash
 site=14
-path=../../results/sitedata/
+path=../../results/testrun_14sites/
 for ((i=1;i<=$site;i++));do (
 	cd R_program_$i
 	mv Output_site* $path
