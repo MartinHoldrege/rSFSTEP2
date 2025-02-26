f687d2df6272c058ccf9c37f7ce0d3af33d7a3ec
Martin Holdrege <martinholdrege@gmail.com>
2025-02-26 16:37:50 -0600

    CO2 fertilization run

--- R_program/Main.R
+++ R_program/Main.R
@@ -29,13 +29,13 @@ simyears <- 150
 # If you would like to implement the effects of CO2 for each climate 
 # scenario, set this boolean to TRUE. If you would like to run without CO2 effects, 
 # set this boolean to FALSE.
-co2_effects <- FALSE
+co2_effects <- TRUE
 
 # If you would like to allow C4 grasses to be represented in climatically suitable sites
 # under future conditions where they are currently absent, set this boolean to TRUE. 
 # If you would like to prevent C4 grasses from establishing in climatically suitable sites 
 # under future conditions where they are currently absent, set this boolean to FALSE.
-c4grass_expansion <- FALSE 
+c4grass_expansion <- TRUE
 
 # If you would like to rescale eind based on climate-derived relative abundance, set this boolean to TRUE. 
 # If TRUE, also set the max_eind for each species to the maximum number of individuals that can establish in a given year in any site. 
