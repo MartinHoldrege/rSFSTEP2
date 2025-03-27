aceed51052ab82a5582366a04b9f75a874f60b03
Martin Holdrege <martinholdrege@gmail.com>
2025-03-27 10:30:01 -0500

    fire1_eind1_c4grass0_co20_2503 run

--- R_program/Main.R
+++ R_program/Main.R
@@ -35,7 +35,7 @@ co2_effects <- FALSE
 # under future conditions where they are currently absent, set this boolean to TRUE. 
 # If you would like to prevent C4 grasses from establishing in climatically suitable sites 
 # under future conditions where they are currently absent, set this boolean to FALSE.
-c4grass_expansion <- TRUE
+c4grass_expansion <- FALSE
 
 # If you would like to rescale eind based on climate-derived relative abundance, set this boolean to TRUE. 
 # If TRUE, also set the max_eind for each species to the maximum number of individuals that can establish in a given year in any site. 
